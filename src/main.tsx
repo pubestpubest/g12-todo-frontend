8364b80c47cda4c9b168d1dbcb1c584986210aa3
korarit pannopasri <108105575+HoloLirny@users.noreply.github.com>
2025-08-04 21:29:04 +0700

    feature: fixcolor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "@picocss/pico/css/pico.red.min.css";
+import "@picocss/pico/css/pico.blue.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
